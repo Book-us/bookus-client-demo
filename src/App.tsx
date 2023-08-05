35bfed67714abfc3968301cc899f7cb4988bac02
gunyongbok <qhrrjsdyd123@gmail.com>
2023-08-05 17:14:45 +0900

    feat: Organize file directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { RecoilRoot } from 'recoil';
+import Router from './page/Router';
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <RecoilRoot>
+                <Router />
+            </RecoilRoot>
+        </>
+    );
 }
 
 export default App;
